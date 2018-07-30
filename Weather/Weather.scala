182728bb2c26afc205416311b1e69199263e8ee9
Katherine Kang <nkang0503@ucla.edu>
2018-07-30 15:51:11 -0700

    Create Weather.scala

---
+++ Weather/Weather.scala
@@ -0,0 +1,133 @@
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkContext
+
+object Weather {
+  def main(args: Array[String]) {
+    try {
+      //set up logging
+//      val lm: LogManager = LogManager.getLogManager
+//      val logger: Logger = Logger.getLogger(getClass.getName)
+//      val fh: FileHandler = new FileHandler("myLog")
+//      fh.setFormatter(new SimpleFormatter)
+//      lm.addLogger(logger)
+//      logger.setLevel(Level.INFO)
+//      logger.addHandler(fh)
+      //set up spark configuration
+      val sparkConf = new SparkConf()
+
+      var logFile = ""
+      var local = 0
+      if (args.length < 2) {
+        sparkConf.setMaster("local[6]")
+        sparkConf.setAppName("Inverted Index").set("spark.executor.memory", "2g")
+        logFile = "/home/ali/work/temp/git/bigsift/src/benchmarks/histogrammovies/data/file1s.data"
+      } else {
+
+        logFile = args(0)
+        local = args(1).toInt
+
+      }
+      //set up lineage
+//      var lineage = true
+//      lineage = true
+
+      val ctx = new SparkContext(sparkConf)
+
+
+      //start recording time for lineage
+      /** ************************
+        * Time Logging
+        * *************************/
+//      val jobStartTimestamp = new java.sql.Timestamp(Calendar.getInstance.getTime.getTime)
+//      val jobStartTime = System.nanoTime()
+//      logger.log(Level.INFO, "JOb starts at " + jobStartTimestamp)
+      /** ************************
+        * Time Logging
+        * *************************/
+
+      val lines = ctx.textFile(logFile, 1)
+      val split = lines.flatMap{s =>
+        val tokens = s.split(",")
+        // finds the state for a zipcode
+        var state = zipToState(tokens(0))
+        var date = tokens(1)
+        // gets snow value and converts it into millimeter
+        val snow = convert_to_mm(tokens(2))
+        //gets year
+        val year = date.substring(date.lastIndexOf("/"))
+        // gets month / date
+        val monthdate= date.substring(0,date.lastIndexOf("/")-1)
+        List[((String , String) , Float)](
+          ((state , monthdate) , snow) ,
+          ((state , year)  , snow)
+        ).iterator
+      }
+      val deltaSnow = split.groupByKey().map{ s  =>
+        val delta =  s._2.max - s._2.min
+        (s._1 , delta)
+      }.filter(s => WeatherAnalysis.failure(s._2))
+      val output = deltaSnow.collect
+
+      /** ************************
+        * Time Logging
+        * *************************/
+      println(">>>>>>>>>>>>>  First Job Done  <<<<<<<<<<<<<<<")
+      val jobEndTimestamp = new java.sql.Timestamp(Calendar.getInstance.getTime.getTime)
+      val jobEndTime = System.nanoTime()
+      logger.log(Level.INFO, "JOb ends at " + jobEndTimestamp)
+      logger.log(Level.INFO, "JOb span at " + (jobEndTime - jobStartTime) / 1000 + "milliseconds")
+
+      /** ************************
+        * Time Logging
+        * *************************/
+
+
+      /** ************************
+        * Time Logging
+        * *************************/
+//      val DeltaDebuggingStartTimestamp = new java.sql.Timestamp(Calendar.getInstance.getTime.getTime)
+//      val DeltaDebuggingStartTime = System.nanoTime()
+//      logger.log(Level.INFO, "Record DeltaDebugging + L  (unadjusted) time starts at " + DeltaDebuggingStartTimestamp)
+      /** ************************
+        * Time Logging
+        * *************************/
+
+
+//      val delta_debug = new DDNonExhaustive[String]
+//      delta_debug.setMoveToLocalThreshold(local);
+//      val returnedRDD = delta_debug.ddgen(lines , new Test, new SequentialSplit[String], lm, fh, DeltaDebuggingStartTime)
+
+      /** ************************
+        * Time Logging
+        * *************************/
+//      val DeltaDebuggingEndTime = System.nanoTime()
+//      val DeltaDebuggingEndTimestamp = new java.sql.Timestamp(Calendar.getInstance.getTime.getTime)
+//      logger.log(Level.INFO, "DeltaDebugging (unadjusted) + L  ends at " + DeltaDebuggingEndTimestamp)
+//      logger.log(Level.INFO, "DeltaDebugging (unadjusted)  + L takes " + (DeltaDebuggingEndTime - DeltaDebuggingStartTime) / 1000 + " milliseconds")
+
+      /** ************************
+        * Time Logging
+        * *************************/
+
+      println("Job's DONE!")
+      ctx.stop()
+    }
+  }
+
+  def convert_to_mm(s: String): Float = {
+    val unit = s.substring(s.length - 2)
+    val v = s.substring(0, s.length - 2).toFloat
+    unit match {
+      case "mm" => return v
+      case _ => return v * 304.8f
+    }
+  }
+  def failure(record:Float): Boolean ={
+    record > 6000f
+  }
+  def zipToState(str : String):String = {
+    return (str.toInt % 50).toString
+  }
+
+}
