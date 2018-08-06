0b8d8084c557edaba78e826eb1f97c2a76ee1260
Katherine Kang <nkang0503@ucla.edu>
2018-08-06 15:17:38 -0700

    Update dataGenerator.py

--- StudentInfo/dataGenerator.py
+++ StudentInfo/dataGenerator.py
@@ -1,4 +1,4 @@
-// Modified from BigSift Benchmark data generator
+# Modified from BigSift Benchmark data generator
 
 from random import randint
 import argparse
@@ -7,7 +7,7 @@ import argparse
 
 parser = argparse.ArgumentParser(description=' College students data generator ')
 parser.add_argument('-o', action="store", dest="outputfile", help='provide output file name', default='studentData.txt')
-parser.add_argument('-n', action="store", dest="num", type=int, help="nubmer of rows in the dataset", default=100000)
+parser.add_argument('-n', action="store", dest="num", type=int, help="nubmer of rows in the dataset", default=1000)
 results = parser.parse_args()
 
 f = open(results.outputfile, "w")
@@ -31,25 +31,25 @@ while i != results.num:
     gender = genderList[randint(0, 1)]
     grade = gradeList[randint(0, 3)]
     major = majorList[randint(0, 9)]
-    fault1 = randint(0, 10000000)
-    fault2 = randint(0, 10000000)
+    #fault1 = randint(0, 10000000)
+    #fault2 = randint(0, 10000000)
     if grade == "0":
-        if fault1 <= 100:  # fault version 1000/10000000
-            age = randint(18000, 19000)
-        else:  # correct version
-            age = randint(18, 19)
+    #    if fault1 <= 100:  # fault version 1000/10000000
+    #        age = randint(18000, 19000)
+    #    else:  # correct version
+        age = randint(18, 19)
     elif grade == "1":
         age = randint(20, 21)
     elif grade == "2":
         age = randint(22, 23)
     else:
         age = randint(24, 25)
-    if major == "ComputerScience":
-        if fault2 <= 500:  # fault version 5000/1000000
-            age = 99999999
-    fault3 = randint(0, 5000000)
-    if fault3 <= 500:  # fault version 5000/5000000
-        f.write(firstName + " " + lastName + " " + gender + " " + grade + " " + str(age) + " " + major + "\n")
-    else:
-        f.write(firstName + " " + lastName + " " + gender + " " + str(age) + " " + grade + " " + major + "\n")
+    #if major == "ComputerScience":
+    #    if fault2 <= 500:  # fault version 5000/1000000
+    #        age = 99999999
+    #fault3 = randint(0, 5000000)
+    #if fault3 <= 500:  # fault version 5000/5000000
+    #    f.write(firstName + " " + lastName + " " + gender + " " + grade + " " + str(age) + " " + major + "\n")
+    #else:
+    f.write(firstName + " " + lastName + " " + gender + " " + str(age) + " " + grade + " " + major + "\n")
 f.close()
